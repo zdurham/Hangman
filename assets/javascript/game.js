2ddade07392d68a87651ba9c0e2772864bb69b82
Zach Durham <ZachDurham@dhcp-v80-100294.wireless.unc.edu>
2017-06-01 18:33:48 -0400

    movie selector is working as intended

---
+++ assets/javascript/game.js
@@ -0,0 +1,18 @@
+//---------- Game Setup ----------//
+
+// Alphabet array to pull from
+var alphabet = ["a", "b", "c", "d","e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+// Words program will choose from
+var words = ["the thing", "blade runner", "they live", "aliens"]
+
+// Chooses current word
+var currentWord = words[Math.floor(Math.random() * words.length)];
+//test
+console.log(currentWord)
+
+
+
+
+
+
