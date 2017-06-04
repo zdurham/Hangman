003239db2614702eebde3d52b03b9ca7f64cf2dd
Zach Durham <ZachDurham@Zachs-MacBook-Pro.local>
2017-06-04 19:51:29 -0400

    current word is displaying correctly

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,31 @@
-//---------- Game Setup ----------//
-
-// Alphabet array to pull from
-var alphabet = ["a", "b", "c", "d","e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-
-// Words program will choose from
-var words = ["the thing", "blade runner", "they live", "aliens"]
-
-// Chooses current word
+//---------- Variables and Setup ----------//
+var words = ["the thing", "bladerunner", "they live", "aliens"];
 var currentWord = words[Math.floor(Math.random() * words.length)];
-//test
-console.log(currentWord)
+var wins = 0; // Tracks wins
+var losses = 0; // Tracks losses
+var guessBank = []; // Will track incorrect guesses
+var correctLetters;
 
 
 
 
 
+function setup() {
+	
+	// Displays current word
+	correctLetters = document.getElementById("currentWord");
+	correctLetters.innerHTML = "<h2 class='currentWord'>Current Word: </h2>";
 
+	for (var i = 0; i < currentWord.length; i++) {
+		correctLetter = "<h2 id='correctLetters'>" + currentWord.charAt(i) + "</h2>"
+		correctLetters.insertAdjacentHTML('beforeend', correctLetter)
+	}
+}
+
+
+
+
+//---------- Object definition begins ----------//
+
+
+//---------- Run Object Functions ----------//
