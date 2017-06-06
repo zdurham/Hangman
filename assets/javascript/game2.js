2eccc40fec2514ed813d18e8254204c452987d35
Zach Durham <ZachDurham@dhcp-v80-100294.wireless.unc.edu>
2017-06-06 13:19:44 -0400

    game functions properly

--- assets/javascript/game2.js
+++ assets/javascript/game2.js
@@ -1,11 +1,11 @@
 // Function will run when page has loaded
 window.onload = function() {
 	//---------- Variables ----------//
-	var words = ["bladerunner"];
-	var alphabet = "abcdefghijklmnopqrstuvwxyc"
+	var words = ["bladerunner", "aliens"];
+	var alphabet = "abcdefghijklmnopqrstuvwxyz"
 	var currentWord = words[Math.floor(Math.random() * words.length)];
-	var winCounter = 0;
-	var lossCounter = 0;
+	var winCounter = 0; //counts wins
+	var lossCounter = 0; //counts losses
 	var guessBank = []; // Array of incorrect guesses
 	var guessNum = 10; // Actual number of guesses that remain
 	var guessRemain; // Grabs "guessesRemaining"
@@ -28,7 +28,7 @@ window.onload = function() {
 
 		// This bit displays remaining guesses
 		guessRemain = document.getElementById("guessesRemaining")
-		guessRemain.innerHTML = "<h2>You have " + guessNum + " guesses remaining!</h2>";
+		guessRemain.innerHTML = "<h2>You have " + guessNum + " lives remaining!</h2>";
 
 		// This bit displays incorrect guesses
 		guessedLetters = document.getElementById("guessedLetters")
@@ -36,35 +36,69 @@ window.onload = function() {
 	}
 
 
+	//---------- GameOver Function ----------//
+	function gameOver(win) {
+		if (win) {
+			alert("We got em!")
+		}
+
+		else {
+			alert("Game over man! Game over!")
+		}
+
+	}
+
 	//---------- Input Function ----------//
-	document.onkeyup = function() {
+	document.onkeyup = function input() {
 		guess = event.key.toLowerCase();
-		oldGuess = false; //This value is used to check if the value has been guessed
 
 		/* Check to make sure the input is a letter first. If yes, then continue with the rest of the function */
 		if (alphabet.indexOf(guess) > -1) {
 
-			// Check if guess is old
-			if ((guessBank.indexOf(guess) > -1) || matchedLetters.indexOf(guess)) 
+			// Check if guess is old. If old, stop
+			if (guessBank.indexOf(guess) > -1) {
+				alert("You have already guessed that letter!");
+				
 
+			}
 
-				// Check if guess matches the letters of currentWord
-				for (var i = 0; i < currentWord.length; i++) {
-					if (guess === currentWord[i]) {
-						lettersMatched++;
-						var showLetter = document.getElementById("l" + i)
-						console.log(showLetter)
-						showLetter.classList.add("correct")
+			// If guess is new, then proceed
+			else if (guessBank.indexOf(guess) <= -1) {
 
+				// Push guess to guessBank
+				guessBank.push(guess)
+				console.log(guessBank)
+
+				if (currentWord.indexOf(guess) > -1) {
+					// Check if guess matches the letters of currentWord
+					for (var i = 0; i < currentWord.length; i++) {
+
+						if (guess === currentWord[i]) {
+							lettersMatched++;
+							var showLetter = document.getElementById("l" + i)
+							console.log(showLetter)
+							showLetter.classList.add("correct")
+							console.log(lettersMatched)
+							if (lettersMatched === currentWord.length) {
+								gameOver(true)
+							}
+						}			
 					}
 				}
 
-
+				else if (currentWord.indexOf(guess) <= -1) {
+					guessNum--;
+					guessRemain.innerHTML = "<h2>You have " + guessNum + " lives remaining!</h2>"
+					guessedLetters.innerHTML += "<h2 id='incorrectLetters'>" + guess + "</h2>";
+					if (guessNum === 0) {
+						gameOver();
+					}
+				}
+			}
 		}
 	}
 
 
-
-
+// Run setup
 setup();
 } 
